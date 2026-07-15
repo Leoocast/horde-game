61bb3d4866b6d90d102e2cf885f6683cf2035509
Leo <leoocast@gmail.com>
2026-07-15 16:40:31 -0700

    stable

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -95,7 +95,7 @@ export function Board({ playerName, setupTurns, onReturnToMenu }: Props) {
       <ToastStack />
       <TutorialGuide game={game} onReturnToMenu={onReturnToMenu} />
       <div className="grid h-[calc(100vh-56px)] grid-cols-1 gap-3 overflow-hidden px-3 pb-40 pt-3">
-        <section className="old-panel battlefield-board-grid p-3">
+        <section className="battlefield-board-grid">
           <div className="battlefield-side battlefield-side-horde">
             <Battlefield game={game} side="horde" cards={game.horde.battlefield} />
           </div>
